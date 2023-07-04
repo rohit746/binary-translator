87a752e7fdcfa3868b0503e8da4a466a56df7da1
Rohit Jadhav <rjjadhav746@gmail.com>
2023-07-04 21:56:18 +0530

    Add text to binary and vice versa

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
 function translateBinaryToAscii(binary: string): string {
-    return  String.fromCharCode(...binary.split(" ").map(function (char){
-        return parseInt(char, 2)
-    }))
+    return String.fromCharCode(
+        ...binary.split(" ").map(function (char) {
+            return parseInt(char, 2);
+        })
+    );
+}
+
+function translateTextToBinary(text: string): string {
+    return text
+        .split("")
+        .map(function (char) {
+            return char.charCodeAt(0).toString(2).padStart(8, "0");
+        })
+        .join(" ");
 }
 
 function handleTranslate() {
-    const binaryInput = (document.getElementById('binaryInput') as HTMLTextAreaElement).value;
-    (document.getElementById('asciiOutput') as HTMLTextAreaElement).value = translateBinaryToAscii(binaryInput);
+    const inputTypeSelect = document.getElementById("inputTypeSelect") as HTMLSelectElement;
+    const inputArea = document.getElementById("inputArea") as HTMLTextAreaElement;
+    const outputArea = document.getElementById("outputArea") as HTMLTextAreaElement;
+
+    const selectedOption = inputTypeSelect.value;
+    const inputValue = inputArea.value.trim();
+
+    if (selectedOption === "binary") {
+        outputArea.value = translateBinaryToAscii(inputValue);
+    } else if (selectedOption === "text") {
+        outputArea.value = translateTextToBinary(inputValue);
+    }
 }
 
-document.getElementById('translateButton')?.addEventListener('click', handleTranslate);
+const translateButton = document.getElementById("translateButton");
+if (translateButton) {
+    translateButton.addEventListener("click", handleTranslate);
+}
