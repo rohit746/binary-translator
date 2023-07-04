cc6189439bef2b909f87e6c20918e73c1af134d2
Rohit Jadhav <rjjadhav746@gmail.com>
2023-07-04 22:30:53 +0530

    copy button

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ function translateTextToBinary(text: string): string {
         .join(" ");
 }
 
-function handleTranslate() {
+function handleTranslate(): void {
     const inputTypeSelect = document.getElementById("inputTypeSelect") as HTMLSelectElement;
     const inputArea = document.getElementById("inputArea") as HTMLTextAreaElement;
     const outputArea = document.getElementById("outputArea") as HTMLTextAreaElement;
@@ -30,7 +30,33 @@ function handleTranslate() {
     }
 }
 
+function handleCopyInput(): void {
+    const inputArea = document.getElementById("inputArea") as HTMLTextAreaElement;
+
+    inputArea.select();
+    inputArea.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
+
+function handleCopyOutput(): void {
+    const outputArea = document.getElementById("outputArea") as HTMLTextAreaElement;
+
+    outputArea.select();
+    outputArea.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
+
 const translateButton = document.getElementById("translateButton");
 if (translateButton) {
     translateButton.addEventListener("click", handleTranslate);
+}
+
+const copyInputButton = document.getElementById("copyInputButton");
+if (copyInputButton) {
+    copyInputButton.addEventListener("click", handleCopyInput);
+}
+
+const copyOutputButton = document.getElementById("copyOutputButton");
+if (copyOutputButton) {
+    copyOutputButton.addEventListener("click", handleCopyOutput);
 }
